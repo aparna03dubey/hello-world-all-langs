78f76eb206effcb68525522c54b1251882524729
Aparna Dubey <101944716+aparna03dubey@users.noreply.github.com>
2022-10-06 10:22:20 +0530

    hello_world_aparna03dubey.cpp
    hello world in hindi.

---
+++ Scripts/hello_world_aparna03dubey.cpp
@@ -0,0 +1,11 @@
+// Programming language: C++
+// Native Language: Hindi
+// Author: Aparna Dubey
+// Github: https://github.com/aparna03dubey
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+cout<<"Namaskar Duniya";
+return 0;
+}
